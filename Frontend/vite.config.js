c6b62f13997f823244bd5c8631c232c82b3b0b88
miznaansari <miznaansari@gmail.com>
2025-03-18 19:52:15 +0530

    url change

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,13 +1,14 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
 import tailwindcss from '@tailwindcss/vite'
+
 export default defineConfig({
-  plugins: [react(),  tailwindcss(),],
-  
+  plugins: [react(), tailwindcss()],
 
   server: {
     host: true,  // Allows access from local network (192.168.x.x)
     port: 3000,  // Change port if needed
     open: true,  // Automatically opens in the browser
+    allowedHosts: ['tinder-1-an6v.onrender.com'], // Allow the specified host
   },
 })
