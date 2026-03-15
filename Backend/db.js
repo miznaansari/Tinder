c699c23684b09ec5274f38c33c0d7a73c45a2cb7
miznaansari <71661652+miznaansari@users.noreply.github.com>
2026-03-15 14:46:24 +0530

    Remove MongoDB connection string from db.js
    Removed sensitive MongoDB connection string from db.js.

--- Backend/db.js
+++ Backend/db.js
@@ -5,7 +5,7 @@ const connectDB = async () => {
   try {
     console.log('MongoDB connecteing');
 
-    await mongoose.connect('mongodb+srv://miznaansari:2sAc7wuwKHHzfnoh@mizna.jfncd.mongodb.net/Tinder?retryWrites=true&w=majority&appName=Mizna', {
+    await mongoose.connect('', {
       useNewUrlParser: true,
       useUnifiedTopology: true
     });
